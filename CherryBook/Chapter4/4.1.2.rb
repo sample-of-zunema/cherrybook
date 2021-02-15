5d7c30b3dfd6b2d30a57b2f973149c2316c55bbd
zunema <yxxa.abc@gmail.com>
2021-02-15 15:49:22 +0000

    RGBカラー変換プログラムの実行例

---
+++ CherryBook/Chapter4/4.1.2.rb
@@ -0,0 +1,16 @@
+# RGBカラー変換プログラムの実行例
+
+
+# a = 0, 0, 0
+# b = 255, 255, 255
+# c = 4, 60,120
+# d = '#00000'
+# e = '#ffffff'
+# f = '#043c78'
+
+puts to_hex(0, 0, 0)
+puts to_hex(255, 255, 255)
+puts to_hex(4, 60,120)
+puts to_ints('#000000')
+puts to_ints('#ffffff')
+puts to_ints('#043c78')
