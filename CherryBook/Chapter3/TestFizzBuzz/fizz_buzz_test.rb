29a310142c8b9136396eba1fa4398787a876ceb5
zunema <yxxa.abc@gmail.com>
2021-02-15 15:13:55 +0000

    プログラム本体とテストコードを分離する

--- CherryBook/Chapter3/TestFizzBuzz/fizz_buzz_test.rb
+++ CherryBook/Chapter3/TestFizzBuzz/fizz_buzz_test.rb
@@ -1,20 +1,5 @@
-# putsメソッドをテストコードに置き換える
-
-
-def fizz_buzz(n)
-  if n % 15 == 0
-    'Fizz Buzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
-  end
-end
-
-
 require 'minitest/autorun'
+require_relative  './fizz_buzz.rb'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
