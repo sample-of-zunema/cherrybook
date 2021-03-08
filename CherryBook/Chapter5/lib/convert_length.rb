afff0661177277edbe39c5670c427678dd7d8ad2
zunema <yxxa.abc@gmail.com>
2021-03-08 07:18:54 +0000

    convert_lengthメソッドを改善しました

--- CherryBook/Chapter5/lib/convert_length.rb
+++ CherryBook/Chapter5/lib/convert_length.rb
@@ -1,4 +1,4 @@
-def convert_length(length, unit_form, unit_to)
+def convert_length(length, from: :m, to: :m)
   units = { m: 1.0, ft: 3.28, in: 39.37 }
-  (length / units[unit_form] * units[unit_to]).round(2)
+  (length / units[from] * units[to]).round(2)
 end
