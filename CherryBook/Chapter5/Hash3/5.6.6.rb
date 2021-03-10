710a4352dfed4a7be2ba0dc5cf7cfdd28cd2d90f
zunema <yxxa.abc@gmail.com>
2021-03-10 13:29:11 +0000

    ハッシュリテラルの{}とブロックの{}

--- CherryBook/Chapter5/Hash3/5.6.6.rb
+++ CherryBook/Chapter5/Hash3/5.6.6.rb
@@ -1,1 +1,20 @@
 # ハッシュリテラルの{}とブロックの{}
+
+def buy_burger(options = {}, menu)
+    puts options
+end
+
+# p buy_burger {'drink' => true, 'potato' => false}, 'fish'
+
+# 第１引数にハッシュの{}が来る場合は（）を省略できない
+p buy_burger({'drink' => true, 'potato' => false}, 'fish')
+
+
+def buy_burger(menu, options = {})
+    puts options
+end
+# 第２引数にハッシュが来る場合は、（）を省略してもエラーにならない
+p buy_burger 'fish', {'drink' => true, 'potato' => false}
+
+# この場合、そもそもハッシュが最後の引数なので、{}を省略することもできる
+p buy_burger 'fush', 'drink' => true, 'potato' => false
