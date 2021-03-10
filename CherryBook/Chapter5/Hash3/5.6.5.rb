d47fff582111cf7f1567c47314e09ff76d4ed09a
zunema <yxxa.abc@gmail.com>
2021-03-10 13:09:45 +0000

    メソッドの呼び出し時の{}の省略

--- CherryBook/Chapter5/Hash3/5.6.5.rb
+++ CherryBook/Chapter5/Hash3/5.6.5.rb
@@ -1,1 +1,26 @@
 # メソッドの呼び出し時の{}の省略
+
+
+# optionsは任意のハッシュを受け付ける
+def buy_burger(menu, options = {})
+    puts options
+end
+
+# ハッシュを第２引数として渡す
+p buy_burger('fish', {'drink' => true, 'potato' => false})
+
+# ハッシュリテラルの{}を省略してメソッドを呼びだす
+p buy_burger('fish', 'drink' => true, 'potato' => false)
+
+
+
+# menuとoptionsの順番を入れ替える
+def buy_burger(options = {}, menu)
+    puts options
+end
+
+# optionsは最後の引数ではないので、ハッシュリテラルの{}はできない
+# p buy_burger('drink' => true, 'potato' => false, 'fish')
+
+# 最後の引数でなければ{}を付けて普通にハッシュを作成する
+p buy_burger({'drink' => true, 'potato' => false}, 'fish')
