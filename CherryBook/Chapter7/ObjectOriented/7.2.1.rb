e253d923d91559df8c945e36144071d00913d10d
zunema <yxxa.abc@gmail.com>
2021-04-16 09:20:20 +0000

    7.2.1.rbの更新

--- CherryBook/Chapter7/ObjectOriented/7.2.1.rb
+++ CherryBook/Chapter7/ObjectOriented/7.2.1.rb
@@ -4,3 +4,8 @@
 users = []
 users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
 users << { first_name: 'Bob', last_name: 'Python', age: 30 }
+
+# ユーザのデータを表示する
+users.each do |user|
+  puts "氏名: #{user[:first_name]} #{user[:last_name]}、年齢:#{user[:age]}"
+end
