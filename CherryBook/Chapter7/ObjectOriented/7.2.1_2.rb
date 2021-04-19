408ae1907ff1f2cf4703af7a86cb0ad7c1de2e61
zunema <yxxa.abc@gmail.com>
2021-04-19 13:24:30 +0000

    7.2.1_2.rb作成

---
+++ CherryBook/Chapter7/ObjectOriented/7.2.1_2.rb
@@ -0,0 +1,13 @@
+# Userクラスを定義する
+
+
+class User
+  attr_reader :first_name, :last_name, :age
+  
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+  
+  
