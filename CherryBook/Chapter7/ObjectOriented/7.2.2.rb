2a20ff6d2371b90129d94a4f27c4fbc399fd2717
zunema <yxxa.abc@gmail.com>
2021-04-21 08:30:02 +0000

    7.2.2.rb作成

--- CherryBook/Chapter7/ObjectOriented/7.2.2.rb
+++ CherryBook/Chapter7/ObjectOriented/7.2.2.rb
@@ -2,3 +2,10 @@
 
 
 # 「Alice Rubyさん、２０歳」というユーザのオブジェクトを作成する
+alice = User.new('Alice', 'Ruby', 20)
+# 「Bob Pythonさん、３０歳」というユーザのオブジェクトを作成する
+bob = User.new('Bob', 'Python', 30)
+
+# どちらもfull_nameメソッドを持つが、保持しているデータが異なるので戻り値は異なる
+p alice.full_name
+p bob.full_name
