4a8dbe8068d3c182c41f94893e1c2c369f18e88b
zunema <yxxa.abc@gmail.com>
2021-04-22 09:07:54 +0000

    7.3.1.rb

--- CherryBook/Chapter7/ClassDefinition/7.3.1.rb
+++ CherryBook/Chapter7/ClassDefinition/7.3.1.rb
@@ -1,10 +1,20 @@
 # オブジェクトの作成とinitializeメソッド
 
+# class User
+#   def initialize
+#     puts 'Initialized.'
+#   end
+# end
+# User.new
+# user = User.new
+# user.initialize
+
+
+# initializeメソッドに引数をつけると、newメソッドを呼ぶ時にも引数が必要になる
 class User
-  def initialize
-    puts 'Initialized.'
+  def initialize(name, age)
+    puts "name: #{name}, age: #{age}"
   end
 end
 # User.new
-user = User.new
-user.initialize
+User.new('Alice', 20)
