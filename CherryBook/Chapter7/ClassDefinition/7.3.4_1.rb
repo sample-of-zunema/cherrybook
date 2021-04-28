1de89813b92fa9cb4cd7423759efb45b8d72dd71
zunema <yxxa.abc@gmail.com>
2021-04-28 13:01:09 +0000

    7.3.4_1.rb

--- CherryBook/Chapter7/ClassDefinition/7.3.4_1.rb
+++ CherryBook/Chapter7/ClassDefinition/7.3.4_1.rb
@@ -4,3 +4,25 @@ class User
   def initialize(name)
     @name = name
   end
+
+  # self.をつけるとクラスメソッドになる
+  def self.create_users(names)
+    # mapメソッドを忘れた人は「4.4.1 map/collect」の項を参照
+    names.map do |name|
+      User.new(name)
+    end
+  end
+  
+  # これはインスタンスメソッド
+  def hello
+    "Hello, I am #{@name}."
+  end
+end
+
+names = ['Alice', 'Bob', 'Carol']
+# クラスメソッドの呼び出し
+users = User.create_users(names)
+users.each do |user|
+  # インスタンスメソッドの呼び出し
+  puts user.hello
+end
